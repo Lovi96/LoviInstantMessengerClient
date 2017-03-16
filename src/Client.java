5871b8fd34399746d1177423f0a4a7fddadd48ad
Marcell Richárd Lovácsi <m.lovacsi@gmail.com>
2017-03-16 23:37:49 +0100

    updated the gui with more features, it also freezes now on connection

--- src/Client.java
+++ src/Client.java
@@ -18,14 +18,61 @@ public class Client extends JFrame {
 	private ObjectOutputStream output;
 	private ObjectInputStream input;
 	private String message = "";
-	private String serverIP;
+	private String serverIP = "127.0.0.1";
 	private Socket connection;
+	private JTextField userNameField;
+	private String actualUserName = "Client";
+	private JLabel userNameLabel;
+	private JButton userNameSetter;
+	private JTextField ipTextField;
+	private JButton ipOkButton;
+	private JLabel ipLabel;
+	private JPanel panel1;
+	private JPanel panel2;
+	private Container contentPane;
+	private JScrollPane scrollPane;
 
-
-	public Client(String host) {
+	public Client() {
 		super("Lovi's instant messenger client");
-		serverIP = host;
+		ipTextField = new JTextField("127.0.0.1");
 		userText = new JTextField();
+		userNameField = new JTextField("Enter username");
+		userNameLabel = new JLabel("Current username: Client");
+		userNameField.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setUserName(e.getActionCommand());
+				userNameField.setText("Username set!");
+			}
+		});
+		userNameSetter = new JButton("Set");
+		userNameSetter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setUserName(userNameField.getText());
+			}
+		});
+		chatWindow = new JTextArea();
+		ipLabel = new JLabel("IP to connect to: ");
+		ipOkButton = new JButton("OK");
+		ipOkButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				setServerIpByField(ipTextField.getText());
+				startRunning();
+				ipOkButton.setVisible(false);
+			}
+		});
+		scrollPane = new JScrollPane(chatWindow);
+		scrollPane.setPreferredSize(new Dimension(250,160));
+		scrollPane.setAlignmentX(LEFT_ALIGNMENT);
+		panel1 = new JPanel();
+		panel1.setLayout(new BoxLayout(panel1,BoxLayout.PAGE_AXIS));
+		panel2 = new JPanel();
+		panel2.setLayout(new BoxLayout(panel2,BoxLayout.LINE_AXIS));
+		userNameField.setFocusable(true);
+		userNameField.setRequestFocusEnabled(true);
+		userNameField.requestFocus();
 		userText.setEditable(false);
 		userText.addActionListener(new ActionListener() {
 			@Override
@@ -34,12 +81,24 @@ public class Client extends JFrame {
 				userText.setText("");
 			}
 		});
-		add(userText, BorderLayout.NORTH);
-		chatWindow = new JTextArea();
-		add(new JScrollPane(chatWindow), BorderLayout.CENTER);
-		setSize(300, 150);
+		panel1.add(Box.createRigidArea(new Dimension(0,5)));
+		panel1.add(userNameLabel);
+		panel1.add(Box.createRigidArea(new Dimension(0,5)));
+		panel1.add(userText);
+		panel1.add(scrollPane);
+		panel2.add(userNameField);
+		panel2.add(userNameSetter);
+		panel2.add(ipLabel);
+		panel2.add(ipTextField);
+		panel2.add(ipOkButton);
+		panel1.add(userText);
+		contentPane = getContentPane();
+		contentPane.add(panel2,BorderLayout.NORTH);
+		contentPane.add(panel1,BorderLayout.CENTER);
+		setSize(600, 300);
 		setVisible(true);
 	}
+
 
 	public void startRunning() {
 		try {
@@ -57,6 +116,7 @@ public class Client extends JFrame {
 
 	private void whileChatting() throws IOException {
 		ableToType(true);
+		userNameField.setEditable(false);
 		do {
 			try {
 				message = (String) input.readObject();
@@ -81,6 +141,7 @@ public class Client extends JFrame {
 		output.flush();
 		input = new ObjectInputStream(connection.getInputStream());
 		showMessage("\n You are now connected! \n");
+		setButtonVisible(false);
 	}
 
 	private void showMessage(final String s) {
@@ -99,25 +160,49 @@ public class Client extends JFrame {
 			output.close();
 			input.close();
 			connection.close();
+			userNameField.setEditable(true);
+			setButtonVisible(true);
+			ipOkButton.setVisible(true);
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	private void connectToServer() throws IOException {
-		showMessage("Attempting connection... \n");
+		showMessage("\nAttempting connection... \n");
 		connection = new Socket(InetAddress.getByName(serverIP), 6789);
 		showMessage("Connected to: " + connection.getInetAddress().getHostName());
 	}
 
 	private void sendMessage(String message) {
 		try {
-			output.writeObject("CLIENT - " + message);
+			output.writeObject(actualUserName+" - " + message);
 			output.flush();
-			showMessage("\nCLIENT - " + message);
+			showMessage("\n"+actualUserName+" - " + message);
 		} catch(IOException ioe) {
-			chatWindow.append("\n something went wrong!");
+			chatWindow.append("\n Couldnt send message!!");
 		}
+	}
+	private void setButtonVisible(boolean visible) {
+		userNameSetter.setVisible(visible);
+	}
+	private void setServerIpByField(final String ipAdress) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				serverIP = ipAdress;
+			}
+		});
+	}
+	private void setUserName(final String actionCommand) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				actualUserName = actionCommand;
+				userNameLabel.setText("Current username: "+actualUserName);
+
+			}
+		});
 	}
 
 }
--- src/ClientTest.java
+++ src/ClientTest.java
@@ -5,7 +5,7 @@ import javax.swing.*;
  */
 public class ClientTest {
 	public static void main(String[] args) {
-		Client clio = new Client("127.0.0.1");
+		Client clio = new Client();
 		clio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		clio.startRunning();
 	}
